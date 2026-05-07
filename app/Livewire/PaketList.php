2b6120dfd3735f7f249dc62c361273f332d40e60
ios2 <ios2@jexadev.com>
2026-05-08 02:22:15 +0700

    Import assets frontend and some frontend page

---
+++ app/Livewire/PaketList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class PaketList extends Component
+{
+    public function render()
+    {
+        return view('livewire.paket-list');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Livewire\Beranda;
+use App\Livewire\Gallery;
+use App\Livewire\PaketList;
+use App\Livewire\Testimoni;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/login', function () {
+    return redirect('customer/login');
+})->name('login');
+
+Route::get('/', Beranda::class)->name('beranda');
+Route::get('/testimoni', Testimoni::class)->name('testimoni');
+Route::get('/gallery', Gallery::class)->name('gallery');
+Route::get('/paket-list', PaketList::class)->name('paketumroh');
