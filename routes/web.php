a448be19e7576442b1440d38a0d76b59f21b15ca
ios2 <ios2@jexadev.com>
2026-05-08 18:03:45 +0700

    add logout routes on web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Livewire\Gallery;
 use App\Livewire\PaketList;
 use App\Livewire\Testimoni;
 use Illuminate\Support\Facades\Route;
+use Filament\Http\Controllers\Auth\LogoutController;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -17,3 +18,5 @@ Route::get('/', Beranda::class)->name('beranda');
 Route::get('/testimoni', Testimoni::class)->name('testimoni');
 Route::get('/gallery', Gallery::class)->name('gallery');
 Route::get('/paket-list', PaketList::class)->name('paketumroh');
+
+Route::get('/logout', LogoutController::class)->name('logout');
