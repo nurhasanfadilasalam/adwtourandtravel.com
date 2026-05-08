fb77b39334c16d84fef2472a31e0301af2af83fd
ios2 <ios2@jexadev.com>
2026-05-08 17:35:29 +0700

    php artisan migrate

---
+++ database/migrations/2026_05_08_103402_add_username_to_users_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // $table->string('username')->unique()->after('name');
+            $table->string('username')->nullable()->unique()->after('name');
+
+            // Hapus komentar di bawah jika email tidak lagi wajib diisi
+            $table->string('email')->nullable()->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('username');
+        });
+    }
+};
